3e41bd1646deda943dc7c6b2d22c2ba43d6c7857
Nayan <nayan.pradhan@hotmail.com>
2020-12-20 19:50:31 +0100

    Implementing estimateGaussian

--- week9/assignment-week9/estimateGaussian.m
+++ week9/assignment-week9/estimateGaussian.m
@@ -1,4 +1,4 @@
-function [mu sigma2] = estimateGaussian(X)
+function [mu, sigma2] = estimateGaussian(X)
 %ESTIMATEGAUSSIAN This function estimates the parameters of a 
 %Gaussian distribution using the data in X
 %   [mu sigma2] = estimateGaussian(X), 
@@ -22,12 +22,9 @@ sigma2 = zeros(n, 1);
 %
 
 
+mu = 1/m * sum(X);
 
-
-
-
-
-
+sigma2 = 1/m * sum((X - repmat(mu, m, 1)).^2);
 
 
 % =============================================================
